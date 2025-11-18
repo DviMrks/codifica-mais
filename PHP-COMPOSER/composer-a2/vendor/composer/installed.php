95d4be9dd738cf1a42935329f9eb785cdad81552
Davi Marques Souza <davi2004marques@gmail.com>
2025-11-18 00:10:46 -0300

    Envio_do_exercicio02_Composer

---
+++ PHP-COMPOSER/composer-a2/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '21787584ea75bfc70671f55969ebb74e7be88926',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '21787584ea75bfc70671f55969ebb74e7be88926',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jawira/emoji-catalog' => array(
+            'pretty_version' => 'v17.0.0',
+            'version' => '17.0.0.0',
+            'reference' => '6591023337b62504c69a17fe3b084ebc11500831',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jawira/emoji-catalog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
